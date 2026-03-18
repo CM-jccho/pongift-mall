26bb67cead188e57e2dd88b9664a490ac57a207b
jccho <jaechul.cho@gmail.com>
2026-03-18 17:40:21 +0900

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>PonGift Mall</h1>
+      <p>Welcome to PonGift Mall!</p>
+    </main>
+  );
+}
